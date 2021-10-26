74617689003bb6c132a174d44e54c1c04b37a15b
RoiMitha971 <emile.cometto@gmail.com>
2021-10-26 13:39:46 +0200

    Création de la classe Card : contient le nom, nombre de pièces à gagner, jet de dé à obtenir, le prix, la couleur et la description. Toutes ces variables sont lisibles, mais pas modifiables
    Création de la classe pile :
    contient la carte présente dans la pile ainsi que le nombre de cartes présentes.
    La méthode draw retourne la carte de la pile et décrémente la valeur de nbCard si cette dernière est supérieure à 0, sinon elle ne renvoie rien.

---
+++ Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace  Miniville
+{
+    public class Card
+    {
+        public string name{ get; private set; }
+        public int moneyToEarn { get; private set; }
+        public int dieCondition { get; private set; }
+        public int cost { get; private set; }
+        public ConsoleColor color { get; private set; }
+        public string[] description { get; private set; }
+
+        public Card(string _name, int _moneyToEarn, int _dieCondition, int _cost, ConsoleColor _color,
+            string[] _description)
+        {
+            name = _name;
+            moneyToEarn = _moneyToEarn;
+            dieCondition = _dieCondition;
+            cost = _cost;
+            color = _color;
+            description = _description;
+        }
+
+    }
+}
---
+++ Pile.cs
@@ -0,0 +1,27 @@
+﻿namespace Miniville
+{
+    public class Pile
+    {
+        public Card card { get; private set; }
+        public int nbCard { get; private set; }
+
+        public Pile(Card _card)
+        {
+             card = _card;
+            nbCard = 8;
+        }
+
+        public Card Draw()
+        {
+            if (nbCard > 0)
+            {
+                nbCard--;
+                return card;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
