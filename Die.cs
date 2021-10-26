bc48be150cc316141ea4974549e7b05aa635f473
Joris <91060318+JorisPineda@users.noreply.github.com>
2021-10-26 14:26:33 +0200

    Ajout de la classé Die qui permet de lancer le dé et Game vide pour le moment.

---
+++ Die.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Miniville
+{
+    public class Die
+    {
+        /// <summary>
+        /// Variable indiquant le nombre de face, comme on utilise un D6 ce seras 7 random prends entre x et y-1
+        /// </summary>
+        public static int nbFaces = 7;
+        
+        /// <summary>
+        /// Méthode de lancer pour réaliser le lancer de dé.
+        /// </summary>
+        /// <returns>la face sur laquelle le dé est tombé</returns>
+        public static int Lancer()
+        {
+            Random rand = new Random();
+            int face = rand.Next(1, nbFaces);
+            Console.WriteLine("résultat : {0}",face);
+
+            return (face);
+        }
+    }
+}
