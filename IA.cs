bd0782bf84ccb704acf2b014ccf07d9043567dc0
RoiMitha971 <emile.cometto@gmail.com>
2021-10-26 15:27:37 +0200

    Création de la classe IA : contient la difficulté de l'IA, l'objet Player auquel l'IA correspond, et un Random.
    La méthode Choose lui permet de faire un choix aléatoire parmis les piles non vides et dont elle peut acheter la carte.
    
    La méthode IAPlay va tirer la carte de la pile choisie, et l'ajouter à la main du joueur qui lui correspond

---
+++ IA.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Miniville
+{
+    public class IA
+    {
+
+        private string difficulty;
+        public Player player;
+        private Random random = new Random();
+
+        public IA(Game _game, string _difficulty, Player _player)
+        {
+            
+            difficulty = _difficulty;
+            player = _player;
+        }
+
+        public void IAPlay(Pile[] _piles)
+        {
+            Card choosenCard = _piles[Choose(_piles)].Draw();
+            player.AddCard(choosenCard);
+        }
+
+        public int Choose(Pile[] _piles)
+        {
+            //Déclaration du choix et de la liste des choix possibles;
+            int choice;
+            List<int> possibleIndex = new List<int>();
+            
+            //On parcours les piles du tableau...
+            for (int i = 0; i < _piles.Length; i++)
+            {
+                //Si la pile n'est pas vide et que le prix de la carte est inférieur à l'argent du joueur IA...
+                if (_piles[i].nbCard != 0 && _piles[i].card.cost <= player.pieces)
+                {
+                    //Ajouter l'index correspondant au choix possible
+                    possibleIndex.Add(i);
+                }
+            }
+            //choisir un index aléatoire parmis ceux enregistrés
+            choice = possibleIndex[random.Next(possibleIndex.Count)];
+            
+            //Retourner le choix
+            return choice;
+        }
+    }
+}
