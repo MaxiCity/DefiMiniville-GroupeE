813ac6ca0ac34b4bd782d353d5acd30f744a3cb6
RoiMitha971 <emile.cometto@gmail.com>
2021-10-26 15:49:40 +0200

    Gestion automatique de la description selon la couleur et le montant de la récompense. la description n'est plus nécessaire en argument pour l'instantiation de Card

--- Card.cs
+++ Card.cs
@@ -20,17 +20,36 @@ namespace  Miniville
         public ConsoleColor color { get; private set; }
         
         //Description de la carte(répartie dans un tableau afin de la segmenter)
-        public string[] description { get; private set; }
+        public string[] description { get; private set; } = new string[3];
 
-        public Card(string _name, int _moneyToEarn, int _dieCondition, int _cost, ConsoleColor _color,
-            string[] _description)
+        public Card(string _name, int _moneyToEarn, int _dieCondition, int _cost, ConsoleColor _color)
         {
             name = _name;
             moneyToEarn = _moneyToEarn;
             dieCondition = _dieCondition;
             cost = _cost;
             color = _color;
-            description = _description;
+
+
+            switch (_color)
+            {
+                case ConsoleColor.Cyan:
+                    description[0] = $"Gagnez {_moneyToEarn}$";
+                    description[1] = "S'active tout";
+                    description[2] = "le temps";
+                    break;
+                case ConsoleColor.Green:
+                    description[0] = $"Gagnez {_moneyToEarn}$";
+                    description[1] = "S'active à";
+                    description[2] = "votre tour";
+                    break;
+                case ConsoleColor.Red:
+                    description[0] = $"Volez {_moneyToEarn}$";
+                    description[1] = "S'active au";
+                    description[2] = "tour adverse";
+                    break;
+            }
+
         }
 
     }
