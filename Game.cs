4bfe30dc45508ac3078a950b0cf8329de495ed62
Joris <91060318+JorisPineda@users.noreply.github.com>
2021-10-27 18:18:15 +0200

    Déclaration des attributs en accords avec le diagramme UML. Création des cartes du jeu. Méthode pour le déroulement d'un tour du joueur ou de l'IA. Méthode RunGame pour la boucle de jeu. On gère les conditions de fin.

--- Game.cs
+++ Game.cs
@@ -1,20 +1,64 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Reflection.Metadata.Ecma335;
 
 namespace Miniville
 {
     public class Game
     {
+        #region Attributs
+        
         /// <summary>
-        /// Méthode créant les cartes, les piles, les joueurs, donnant les cartes de départ
+        /// display pour afficher et gérer le jeu
         /// </summary>
-        public void startGame()
+        HMICUI display;
+        
+        
+        /// <summary>
+        /// //Liste des cartes du jeu
+        /// </summary>
+        List<Card> cardsListe = new List<Card>();
+        
+        /// <summary>
+        /// Piles des cartes à acheter
+        /// </summary>
+        Pile[] piles;
+        
+        /// <summary>
+        /// Tableau des joueurs
+        /// </summary>
+        private Player[] players = new Player[2];
+
+        private IA adversaire;
+        
+        /// <summary>
+        /// Pou gérer le dés
+        /// </summary>
+        private Die die = new Die();
+
+        /// <summary>
+        /// int indiquant l'index du joueur dont c'est le tour
+        /// </summary>
+        int actualPlayer = 1;
+
+        /// <summary>
+        /// int indiquant l'index du joueur dont ce n'est pas le tour
+        /// </summary>
+        int otherplayer = 0;
+
+        /// <summary>
+        /// bool arrêtant la boucle de jeu
+        /// </summary>
+        private bool endGame;
+
+        #endregion
+
+        
+        public Game()
         {
-            #region Carte
-            
-            // Création de toutes les cartes et de la liste les contenant
-            List<Card> cardsListe = new List<Card>();
+            display = new HMICUI(this);
             
             Card champsDeBlé = new Card("Champs de blé", 1, 1, 1, ConsoleColor.Cyan);
             cardsListe.Add(champsDeBlé);
@@ -41,7 +85,7 @@ namespace Miniville
             cardsListe.Add(stade);
             
             //Création du tableau des piles de carte
-            Pile[] piles = new Pile[cardsListe.Count];
+            piles = new Pile[cardsListe.Count];
 
             //Création de piles pour chaque carte et ajout dans le tableau
             for (int i = 0; i < piles.Length; i++)
@@ -49,23 +93,124 @@ namespace Miniville
                 piles[i] = new Pile(cardsListe[i]);
             }
             
-            #endregion
-
-            #region création joueur
-            
-            Player player = new Player();
-            player.city.Add(champsDeBlé);
-            player.city.Add(boulangerie);
-            player.UpdateMoney(3);
-
-            Player ia = new Player();
-            IA adversaire = new IA("aléatoire", ia);
-            adversaire.player.city.Add(champsDeBlé);
-            adversaire.player.city.Add(boulangerie);
-            adversaire.player.UpdateMoney(3);
-            
-            #endregion
+            startGame();
         }
+        
+        /// <summary>
+        /// Méthode créant les cartes, les piles, les joueurs, donnant les cartes de départ
+        /// </summary>
+        public void startGame()
+        {
+            #region création joueur
+
+            Player player = new Player();
+            player.city.Add(cardsListe[0]);
+            player.city.Add(cardsListe[2]);
+            player.UpdateMoney(3);
+            players[0] = player;
+            
+            
+            Player ia = new Player();
+            adversaire = new IA(ia);
+            adversaire.player.city.Add(cardsListe[0]);
+            adversaire.player.city.Add(cardsListe[2]);
+            adversaire.player.UpdateMoney(3);
+            players[1] = ia;
+            
+            #endregion
+            
+            display.ChooseName();
+            Console.Clear();
+            
+            display.DisplayCities(players);
+            Console.ReadLine();
+            Console.Clear();
+            
+            RunGame();
+            
+            return;
+        }
+        //Un tour de joueur
+        public void PlayNextTurn(Player[] players)
+        {
+            actualPlayer = actualPlayer == 0 ? 1 : 0;
+            otherplayer = otherplayer == 1 ? 0 : 1;
+
+            bool humanPlayer = actualPlayer == 0;
+
+            //Début du tour lancer de dés
+            int dieResult = Die.Lancer();
+
+            //Résolution des effets de cartes
+            int[] resultActualPlayer = players[actualPlayer].UseCards(true, dieResult);
+            int[] resultOtherPlayer = players[otherplayer].UseCards(false, dieResult);
+            players[otherplayer].UpdateMoney(-resultOtherPlayer[1]);
+            
+            //Affiche les villes des joueurs selon qui est le joueur actuelle et le résultat du dé
+            display.DisplayCities(players, actualPlayer, dieResult);
+
+            display.DisplayTurnResult(resultActualPlayer, resultOtherPlayer, humanPlayer);
+            Console.ReadLine();
+            Console.Clear();
+
+            //Affiche et permet de choisir parmi toutes les piles
+            
+            if (actualPlayer == 0)
+            {
+                int selection = display.Choose(piles);
+                
+                if (selection >= 0)
+                {
+                    players[0].city.Add(piles[selection].Draw());
+                }
+            }
+            else if (actualPlayer == 1)
+            {
+                Card cardChoice = adversaire.IAPlay(piles);
+                display.DisplayIADraw(cardChoice);
+                if (cardChoice != null)
+                {
+                    players[1].city.Add(cardChoice);
+                }
+            }
+
+            //Affichage des villes des deux joueurs
+            display.DisplayCities(players);
+            Console.ReadLine();
+            Console.Clear();
+            return;
+        }
+
+        public void RunGame()
+        {
+            while (endGame == false)
+            {
+                //On lance le tour du joueur 
+                PlayNextTurn(players);
+                EndGame(players[0]);
+            
+                if (EndGame(players[0]))
+                {
+                    display.DisplayEndingMessage(true);
+                    Console.ReadLine();
+                    break;
+                }
+            
+                //playnextturn IA
+                PlayNextTurn(players);
+                EndGame(players[1]);
+            
+                if (EndGame(players[1]))
+                {
+                    display.DisplayEndingMessage(false);
+                    Console.ReadLine();
+                    break;
+                }
+            }
+            
+        }
+        
+        
         /// <summary>
         /// Fonction vérifiant la fin du jeu à 20 pièces
         /// </summary>
@@ -73,11 +218,13 @@ namespace Miniville
         /// <returns>Bool qui si est true met fin au jeu</returns>
         public bool EndGame(Player actualPlayer)
         {
-            bool endGame = false;
-
             if (actualPlayer.pieces >= 20)
             {
                 endGame = true;
+            }
+            else
+            {
+                endGame = false;
             }
 
             return(endGame);
