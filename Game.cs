473ba5899aafb1904ada5f93b44d30b11e7e3fb7
Joris <91060318+JorisPineda@users.noreply.github.com>
2021-10-27 10:49:08 +0200

    Création méthode startGame qui crée les cartes les mets dans une liste et crée le tableau des piles des cartes, crée aussi le joueur avec ses 2 cartes de départs et son argent.

--- Game.cs
+++ Game.cs
@@ -1,9 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Miniville
 {
     public class Game
     {
-        
+        /// <summary>
+        /// Méthode créant les cartes, les piles, les joueurs, donnant les cartes de départ
+        /// </summary>
+        public void startGame()
+        {
+            #region Carte
+            
+            // Création de toutes les cartes et de la liste les contenant
+            List<Card> cardsListe = new List<Card>();
+            
+            Card champsDeBlé = new Card("Champs de blé", 1, 1, 1, ConsoleColor.Cyan);
+            cardsListe.Add(champsDeBlé);
+
+            Card ferme = new Card("Ferme", 1, 1, 2, ConsoleColor.Cyan);
+            cardsListe.Add(ferme);
+            
+            Card boulangerie = new Card("Boulangerie", 2, 2, 1, ConsoleColor.Green);
+            cardsListe.Add(boulangerie);
+            
+            Card café = new Card("Café", 1, 3, 2, ConsoleColor.Red);
+            cardsListe.Add(café);
+
+            Card superette = new Card("Superette", 3, 4, 2, ConsoleColor.Green);
+            cardsListe.Add(superette);
+            
+            Card forêt = new Card("Forêt", 1, 5, 2, ConsoleColor.Cyan);
+            cardsListe.Add(forêt);
+            
+            Card restaurant = new Card("Restaurant", 2, 5, 4, ConsoleColor.Red);
+            cardsListe.Add(restaurant);
+            
+            Card stade = new Card("Stade", 4, 6, 6, ConsoleColor.Cyan);
+            cardsListe.Add(stade);
+            
+            //Création du tableau des piles de carte
+            Pile[] piles = new Pile[cardsListe.Count];
+
+            //Création de piles pour chaque carte et ajout dans le tableau
+            for (int i = 0; i < piles.Length; i++)
+            {
+                piles[i] = new Pile(cardsListe[i]);
+            }
+            
+            #endregion
+
+            Player player = new Player();
+            player.city.Add(champsDeBlé);
+            player.city.Add(boulangerie);
+            player.UpdateMoney(3);
+            
+            
+        }
     }
 }
